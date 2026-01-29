df971bf8f08da437f0c8f6a207fc4fa109e63e3a
Russell J Gomez Santoyo <89620581+russ-cx@users.noreply.github.com>
2026-01-29 10:05:56 -0600

    Basic Ui
    right now its for deployment

---
+++ script.py
@@ -0,0 +1,6 @@
+document.getElementById("sendBtn").addEventListener("click", () => {
+  const input = document.getElementById("userInput").value;
+
+  document.getElementById("outputBox").textContent =
+    "You typed:\n\n" + input + "\n\n(Backend not connected yet)";
+});
