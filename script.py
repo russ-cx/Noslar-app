35c59273713e6f1a61700e8f90246b022acf7435
Russell J Gomez Santoyo <89620581+rufflescx12@users.noreply.github.com>
2026-01-29 10:38:57 -0600

    fix

--- script.py
+++
@@ -1,8 +0,0 @@
-const API_BASE = "http://127.0.0.1:8000";
-
-document.getElementById("sendBtn").addEventListener("click", () => {
-  const input = document.getElementById("userInput").value;
-
-  document.getElementById("outputBox").textContent =
-    "You typed:\n\n" + input + "\n\n(Backend not connected yet)";
-});
